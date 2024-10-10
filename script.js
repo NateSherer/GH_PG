4a21add425f73c5218d7bdddef520293fe99a0aa
Nathaniel L. Sherer <NateSherer@Administrators-MacBook-Pro.local>
2024-10-10 15:49:46 -0400

    added js

---
+++ script.js
@@ -0,0 +1,6 @@
+alert(inputText);
+const searchBar = document.getElemenById("searchBar");
+searchBar.addEventListener("input", (event) => {
+  const searchTerm = event.target.value.toLowerCase();
+  // Search Option
+})
